2a479e1c5113dcf6e78fb7eca86f950053313853
Jean-Michel Nadeau <jeanmichel.nadeau@gmail.com>
2022-06-08 16:40:46 -0400

    Fix date format pour la désérialisation

--- src/main/java/org/kickmyb/server/ConfigJSON.java
+++ src/main/java/org/kickmyb/server/ConfigJSON.java
@@ -1,11 +1,13 @@
 package org.kickmyb.server;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import org.joda.time.DateTime;
 import org.kickmyb.CustomGson;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Type;
 
 // le but est de forcer un seul format compatible avec Java de Android par défaut
 // https://github.com/google/gson/blob/master/gson/src/main/java/com/google/gson/internal/bind/DateTypeAdapter.java
@@ -17,7 +19,15 @@ public class ConfigJSON {
 
     @Bean
     public Gson gson() {
-        return CustomGson.getIt();
+        String dateFormat = "yyyy-MM-dd HH:mm:ss Z";
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.enableComplexMapKeySerialization();
+        builder.setDateFormat(dateFormat);
+
+        builder.registerTypeAdapter(byte[].class, new CustomGson.ByteArraySerialiser());
+        builder.setPrettyPrinting();
+        return builder.create();
     }
 
 }
